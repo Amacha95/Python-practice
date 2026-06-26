f9ff2f22e4ba8bc90512ef184865fbb2570d4219
Amacha95 <schadrackahlonsou@gmail.com>
2026-06-25 23:56:16 -0700

    Refactor Calculator.py for improved output formatting

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,11 @@
-print("Welcome to the my Calculator!")
+print("Welcome to my Calculator!")
 
-num1 =input("enter the first number")
-num2 =input("enter the second number")
-num3 =input("enter the third number")
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+num3 = input("Enter the third number: ")
 
 num1 = int(num1)
-num2= int(num2)
+num2 = int(num2)
 num3 = int(num3)
 
 add = num1 + num2 + num3
@@ -13,7 +13,7 @@ subtract = num1 - num2 - num3
 multiply = num1 * num2 * num3
 divide = num1 / num2 / num3
 
-print("addition", add)
-print("subtraction", subtract)
-print("multiplication", multiply)
-print("division", divide)
+print("Addition:", add)
+print("Subtraction:", subtract)
+print("Multiplication:", multiply)
+print("Division:", divide)
